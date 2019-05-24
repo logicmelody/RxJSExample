a93a748a23675abbc0b3bd6968c1e29f70cd4665
Danny Lin <danny.l@ljit.io>
2019-05-24 19:01:23 +0800

    Click to plus / minus example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 import {
 	fromEvent,
+	merge,
 } from "rxjs";
 
-console.log("Test");
+import {
+	map,
+	mapTo,
+	scan,
+} from "rxjs/operators";
+
+// fromEvent(window, 'click').pipe(
+// 	map(e => e.target),
+// ).subscribe(value => {
+// 	console.log(value);
+// })
+
+
+// fromEvent(window, 'click').pipe(
+// 	map(e => 1),
+// 	scan((total, now) => total + now),
+// ).subscribe(value => {
+// 	document.getElementById('counter').innerText = 'count = ' + value;
+// })
+
+
+const plusObservable = fromEvent(document.getElementById('plus-button'), 'click').pipe(
+	mapTo(1)
+);
+const minusObservable = fromEvent(document.getElementById('minus-button'), 'click').pipe(
+	mapTo(-1)
+);
+
+merge(plusObservable, minusObservable).pipe(
+	scan((total, now) => total + now),
+).subscribe(value => {
+	document.getElementById('counter').innerText = 'count = ' + value;
+});
